6c85ff4f54cb0dd7ade8a5289ffdfa02a0618f3f
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-17 20:45:14 +0300

    Data types

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,12 +1,19 @@
 CREATE TABLE users_with_emails();
 
-DROP TABLE users, users_with_emails;
+DROP TABLE users;
 
 CREATE TABLE users(
     first_name varchar(64), -- 1 symbol = 1 byte
     last_name varchar(64),
-    biography text
+    biography text,
+    gender varchar(30),
+    is_subscribe boolean,
+    birthday date,
+    foot_size smallint,
+    height numeric(4, 1) -- numeric(точність, масштаб)
 );
+
+-- 123.56 // точність - 5 // масштаб - 2
 
 /*
 
