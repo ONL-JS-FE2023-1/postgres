1448db5c6022bd8c419f14d5b3d7a412694e5934
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-17 20:29:48 +0300

    Creating table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,1 +1,18 @@
-CREATE TABLE test();
+CREATE TABLE users_with_emails();
+
+DROP TABLE users, users_with_emails;
+
+CREATE TABLE users(
+    first_name varchar(64), -- 1 symbol = 1 byte
+    last_name varchar(64),
+    biography text
+);
+
+/*
+
+char(5): '1234567' -> error, '12' -> '12   '
+varchar(5): '1234567' -> error, '12' -> '12'
+
+text -> max 1GB
+
+*/
