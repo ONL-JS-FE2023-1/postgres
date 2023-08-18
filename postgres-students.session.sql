0b39abda5a6901b3982b7c6708181b40cf24637a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-18 18:33:07 +0300

    Inserting data into a table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -3,23 +3,19 @@ CREATE TABLE users_with_emails();
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name varchar(64), -- 1 symbol = 1 byte
+    first_name varchar(64), 
     last_name varchar(64),
     biography text,
     gender varchar(30),
     is_subscribe boolean,
     birthday date,
     foot_size smallint,
-    height numeric(4, 1) -- numeric(точність, масштаб)
+    height numeric(4, 1) 
 );
 
--- 123.56 // точність - 5 // масштаб - 2
-
-/*
-
-char(5): '1234567' -> error, '12' -> '12   '
-varchar(5): '1234567' -> error, '12' -> '12'
-
-text -> max 1GB
-
-*/
+INSERT INTO users VALUES
+                ('John','Doe','biography............','male',true,'2023-08-18',42,2.1),
+                ('Jane','Doe','biography............','male',false,'2023-08-18',42,2.1),
+                ('Clark','Doe','biography............','male',false,'2023-08-18',42,2.1),
+                ('Donald','Doe',NULL,'male',true,'2023-08-18',NULL,NULL)
+                ;
