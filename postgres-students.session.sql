4977d6901cc08685e325189671f62c060a6b21a0
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-22 20:23:35 +0300

    1:1

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,38 +1,15 @@
--- Реалізувати сутність контенту
--- У контенту є:
--- назва
--- опис
--- автор (юзер, який створив цей контент)
--- таймстемп створення
+----- 1:1
 
--- Реакції
--- is_liked
-        -- null - користувач не ставив оцінку
-        -- true - контент лайкнули
-        -- false -- контент дизлайкнули
-
-
--- У контента може бути багато реакцій від користувачів
--- Реакції - зв'язок між користувачем і контентом
-
-
-CREATE TABLE contents(
+CREATE TABLE coaches(
     id serial PRIMARY KEY,
-    name varchar(256) CHECK (name != '') NOT NULL,
-    description text,
-    author_id int REFERENCES users(id),
-    created_at timestamp DEFAULT current_timestamp
+    name varchar(256) CHECK(name != '') NOT NULL
 );
 
-INSERT INTO contents(name, description, author_id) VALUES
-('funny dogs', NULL, 4);
-
-CREATE TABLE reactions(
-    content_id int REFERENCES contents(id),
-    user_id int REFERENCES users(id),
-    is_liked boolean
+CREATE TABLE teams(
+    id serial PRIMARY KEY,
+    name varchar(256) CHECK(name != '') NOT NULL,
+    coach_id int REFERENCES coaches(id)
 );
 
-INSERT INTO reactions VALUES(
-    1, 2, false
-);
+ALTER TABLE coaches
+ADD COLUMN team_id int REFERENCES teams(id);
