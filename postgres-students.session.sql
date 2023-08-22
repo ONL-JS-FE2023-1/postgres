d12e634aa05901d5cacd16114b58a4f176ea4f5c
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-22 20:08:03 +0300

    REactions

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,44 +1,38 @@
--- Реалізувати чат між юзерами
--- В кожного чата є певний власник
--- В кожного юзера може бути багато чатів (юзер може состоять в багатьох чатах)
--- В кожному чаті - багато повідомлень. Одне повідомлення - в одному чаті
+-- Реалізувати сутність контенту
+-- У контенту є:
+-- назва
+-- опис
+-- автор (юзер, який створив цей контент)
+-- таймстемп створення
 
-CREATE TABLE chats(
+-- Реакції
+-- is_liked
+        -- null - користувач не ставив оцінку
+        -- true - контент лайкнули
+        -- false -- контент дизлайкнули
+
+
+-- У контента може бути багато реакцій від користувачів
+-- Реакції - зв'язок між користувачем і контентом
+
+
+CREATE TABLE contents(
     id serial PRIMARY KEY,
     name varchar(256) CHECK (name != '') NOT NULL,
-    owner_id int REFERENCES users(id),
+    description text,
+    author_id int REFERENCES users(id),
     created_at timestamp DEFAULT current_timestamp
 );
 
-INSERT INTO chats (name, owner_id) VALUES
-('superchat', 1);
+INSERT INTO contents(name, description, author_id) VALUES
+('funny dogs', NULL, 4);
 
-CREATE TABLE chats_to_users(
-    chat_id int REFERENCES chats(id),
+CREATE TABLE reactions(
+    content_id int REFERENCES contents(id),
     user_id int REFERENCES users(id),
-    join_at timestamp DEFAULT current_timestamp,
-    PRIMARY KEY(chat_id, user_id)
+    is_liked boolean
 );
 
-INSERT INTO chats_to_users(chat_id, user_id) VALUES
-(2, 3);
-
-INSERT INTO chats_to_users(chat_id, user_id) VALUES
-(2, 1);
-
-
-CREATE TABLE messages(
-    id serial PRIMARY KEY,
-    body text CHECK (body != '') NOT NULL,
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean DEFAULT false NOT NULL,
-    author_id int,
-    chat_id int,
-    FOREIGN KEY (chat_id, author_id) REFERENCES chats_to_users(chat_id, user_id) 
+INSERT INTO reactions VALUES(
+    1, 2, false
 );
-
-INSERT INTO messages(body, author_id, chat_id) VALUES
-('Hi', 3, 2),
-('Hi Clark', 1, 2),
-('Go for coffee?', 1, 2),
-('Go', 3, 2);
