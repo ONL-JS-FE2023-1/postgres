caa5ea98220106d746f3d75114bbe06be18fa710
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-23 20:55:00 +0300

    getUsers (1/2)

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
 const { Client } = require('pg');
 const { mapUsers } = require('./utils');
-
-const configs = {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'students'
-}
+const { configs } = require('./configs');
+// const { loadUsers } = require('./api');
+const { getUsers } = require('./api/fetch');
 
 const client = new Client(configs);
-
-const usersArray = [
-    {
-        firstName: 'Test1',
-        lastName: 'Grishina',
-        email: 'test1@gmail.com',
-        isSubscribe: false,
-        gender: 'female'
-    },
-    {
-        firstName: 'Test2',
-        lastName: 'Grishina',
-        email: 'test2@gmail.com',
-        isSubscribe: false,
-        gender: 'female'
-    },
-    {
-        firstName: 'Test3',
-        lastName: 'Grishina',
-        email: 'test3@gmail.com',
-        isSubscribe: false,
-        gender: 'female'
-    },
-    {
-        firstName: 'Test4',
-        lastName: 'Grishina',
-        email: 'test4@gmail.com',
-        isSubscribe: false,
-        gender: 'female'
-    },
-    {
-        firstName: 'Test5',
-        lastName: 'Grishina',
-        email: 'test5@gmail.com',
-        isSubscribe: false,
-        gender: 'female'
-    }
-]
 
 async function start() {
     await client.connect();
@@ -62,4 +19,8 @@ async function start() {
     console.log(response);
 }
 
-start();
+//start();
+
+// loadUsers();
+
+console.log(getUsers());
