818378e856f5026a517b52b5c37e1e0a2e17653d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-28 18:38:33 +0300

    Aliases

--- select.sql
+++ select.sql
@@ -1,28 +1,22 @@
--- age(date) - визначає кількість років яка минула з переданої дати
--- extract("що_саме_витягаємо" from interval) - витягає з інтервалу потрібну вам інформацію
 
-SELECT id, first_name, last_name, extract("years" from age(birthday)) FROM users;
-
--- make_interval(years, months, days) - функція, яка створює інтервали
-SELECT id, first_name, last_name, make_interval(20, 8) FROM users;
-
-
--- Знайти всіх користувачів, старших за 45 років
-SELECT first_name, last_name, birthday, extract("years" from age(birthday)) FROM users
-WHERE extract("years" from age(birthday)) > 45;
-
-
--- Знайти всіх користувачів, які народились у вересні
-SELECT first_name, last_name, birthday FROM users
-WHERE extract("months" from birthday) = 9;
 
 
 -- Знайти всіх користувачів, від 20 до 40 років
 SELECT first_name, last_name, birthday, extract("years" from age(birthday)) FROM users
 WHERE extract("years" from age(birthday)) BETWEEN 20 AND 40;
 
--- Для всіх користувачів, які старші за 65 років підписати на розсилку
-UPDATE users
-SET is_subscribe = true
-WHERE extract("years" from age(birthday)) > 65
-RETURNING first_name, last_name, extract("years" from age(birthday));
+-- Перейменувати extract на age
+SELECT first_name, last_name, birthday, extract("years" from age(birthday)) AS age FROM users
+WHERE extract("years" from age(birthday)) BETWEEN 20 AND 40;
+
+-- Аліас - псевдонім -- AS
+
+SELECT first_name AS "Ім'я",
+last_name AS "Прзівище",
+id AS "Особистий номер"
+FROM users; 
+
+
+-- Аліаси для таблиць
+
+SELECT * FROM orders AS "Чек";
