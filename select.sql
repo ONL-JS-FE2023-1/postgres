d31ad94edd254601b8412183a7147cd014f201e5
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-28 18:25:49 +0300

    Date (intervals)

--- select.sql
+++ select.sql
@@ -1,131 +1,28 @@
--- SELECT назви_стовпців_які_вам_потрібні FROM назва_таблиці
+-- age(date) - визначає кількість років яка минула з переданої дати
+-- extract("що_саме_витягаємо" from interval) - витягає з інтервалу потрібну вам інформацію
 
-SELECT * FROM users; -- вибірка всіх юзерів (всі стовпці)
+SELECT id, first_name, last_name, extract("years" from age(birthday)) FROM users;
 
-SELECT id, email FROM users; -- вибірка всіх юзерів (специфікуємо стовпці)
-
-SELECT id, email FROM users WHERE id >= 900; -- вибірка за умовою
-
-SELECT id, email, gender FROM users WHERE gender = 'female'; -- вибірка за умовою
-
--- Знайти всіх користувачів, які підписані на наші новини
-SELECT id, email, is_subscribe FROM users WHERE is_subscribe;
-
--- Знайти всіх користувачів, у яких висота не NULL
-SELECT id, email, height FROM users WHERE height IS NOT NULL;
---                                      висота != NULL
+-- make_interval(years, months, days) - функція, яка створює інтервали
+SELECT id, first_name, last_name, make_interval(20, 8) FROM users;
 
 
--- Вибір всіх користувачів, у яких парний id
-SELECT id FROM users WHERE id % 2 = 0;
+-- Знайти всіх користувачів, старших за 45 років
+SELECT first_name, last_name, birthday, extract("years" from age(birthday)) FROM users
+WHERE extract("years" from age(birthday)) > 45;
 
 
--- Вибір всіх юзерів, в кого зріст більше 1.5
-SELECT id, email, height FROM users WHERE height > 1.5;
+-- Знайти всіх користувачів, які народились у вересні
+SELECT first_name, last_name, birthday FROM users
+WHERE extract("months" from birthday) = 9;
 
 
--- Вибір всіх юзерів чоловічої статі, які підписані на наші новини
-SELECT id, email, gender, is_subscribe FROM users WHERE gender = 'male' AND is_subscribe;
+-- Знайти всіх користувачів, від 20 до 40 років
+SELECT first_name, last_name, birthday, extract("years" from age(birthday)) FROM users
+WHERE extract("years" from age(birthday)) BETWEEN 20 AND 40;
 
--- CRUD (в контексті реляційних БД)
--- create - CREATE
--- read - SELECT
--- update - UPDATE
--- delete - DELETE, TRUNCATE
-
-
--- Знайти всіх юзерів з якимось певним іменем
-SELECT id, first_name FROM users WHERE first_name = 'William';
-
--- Знайти всіх юзерів, з іменем Уільям або Джон або Тест
-SELECT id, first_name FROM users WHERE first_name IN('William', 'John', 'Test');
-
-
--- Знайти всіх юзерів, у яких id між 460 і 500 (включно)
--- 1 шлях
-SELECT id, email FROM users WHERE id >= 460 AND id <= 500;
--- 2 шлях
-SELECT id, email FROM users WHERE id BETWEEN 460 AND 500;
-
--- Знайти всіх користувачів, у яких ріст в проміжку між 1.5 і 2
-SELECT id, email, height FROM users WHERE height BETWEEN 150 AND 200;
-
-
--- Знайти всіх юзерів, ім'я яких починається на букву "J"
-SELECT first_name FROM users WHERE first_name LIKE 'J%';
--- LIKE - задавання шаблону // РЕГІСТР МАЄ ЗНАЧЕННЯ!
--- "%" - будь-яка кількість будь-яких символів
--- "_" - 1 будь-який символ
-
--- Знайти всіх користувачів, у яких рівно 5 символів у імені
-SELECT first_name FROM users WHERE first_name LIKE '_____';
-
--- Знайти всіх користувачів, у яких ім'я складається з 3 символів і починається на "A"
-SELECT first_name FROM users WHERE first_name LIKE 'A__';
-
--- Знайти всіх користувачів, у яких ім'я закінчується на "a"
-SELECT first_name FROM users WHERE first_name LIKE '%a';
-
-
-
-----------
-
-
-ALTER TABLE users
-ADD COLUMN weight int CHECK(weight != 0 AND weight > 0);
-
-
+-- Для всіх користувачів, які старші за 65 років підписати на розсилку
 UPDATE users
-SET weight = 60;
-
-
-----------
-
-
--- Оновлюємо вагу: для жінок - 60, для чоловіків - 80
-UPDATE users
-SET weight = 60
-WHERE gender = 'female';
-
-UPDATE users
-SET weight = 80
-WHERE gender = 'male';
-
-SELECT gender, weight FROM users WHERE gender = 'male';
-
--- Вcтановити Джону Доу (id 1) вагу 100
-UPDATE users
-SET weight = 100
-WHERE id = 1;
-
--- Змінити вагу для юзерів (вага - 20 кг) у яких прізвище починається на 'J' або на 'K'
-UPDATE users
-SET weight = weight - 20
-WHERE last_name LIKE 'J%' OR last_name LIKE 'K%'
-RETURNING last_name, weight;
-
--- Видалити якогось певного юзера в повернути всі стовпці цього юзера
-INSERT INTO users (
-    first_name,
-    last_name,
-    email,
-    gender,
-    is_subscribe,
-    birthday,
-    foot_size,
-    height,
-    weight
-  )
-VALUES (
-    'Test',
-    'Testovich',
-    'testttttt@gmail.com',
-    'male',
-    true,
-    '1990-08-08',
-    '45',
-    190.6,
-    44
-  ) RETURNING *;
-
-DELETE FROM users WHERE id = 988 RETURNING *;
+SET is_subscribe = true
+WHERE extract("years" from age(birthday)) > 65
+RETURNING first_name, last_name, extract("years" from age(birthday));
