0ac428f6ef84567b59f083ec243259a5a1fd6e40
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-04 18:40:58 +0300

    Home practice resolve

--- normalize.sql
+++ normalize.sql
@@ -1,46 +1,77 @@
-------Pizza Delivery-----
-
-
 /*
 
-Ресторани готують піцци
-Служби доставки розвозять піцци
-Ресторанів багато, ресторан зв'язаний зі службами доставки, які працюють в конкретних районах
-Служби доставки можуть працювати одночасно по декількох районах
+Необхідно спроектувати БД поставки товарів.
+
+В БД має зберігатися наступна інформація.
+
+Товари:
+- код товару
+- назва товару
+- ціна
+
+Замовлення на поставку:
+- код замовленння
+- найменування постачальника
+- адреса постачальнику
+- номер договору
+- дата заключення договору
+- найменування товару
+- план поставки (кількість в шт)
+
+Фактичні відгрузки товару:
+- код відгрузки
+- код замовлення
+- дата відгрузки
+- відгружено товару (шт)
+
+При проектуванні БД потрібно врахувати:
+- товар має декілька замовлень на постачання
+- замовлення відповідає одному товару
+- товару можуть відповідати декілька відгрузок
+- у одній відгрузці може бути декілька товарів
+- товар не обов'язково має замовлення; кожне замовлення відповідає товару
+- товар не обов'язково відгружається замовнику, але кожна відгрузка обов'язково відповідає певному товару
+
+Примітка 1.
+ПЛАН ПОСТАВКИ: у вас може бути замовлення, але ви його можете виконати не одразу, а частинами
+
+Примітка 2.
+Структура відношень у завданні є орієнтовною)
+Таблиці потрібно спроектувати самостійно, не бійтеся нормалізувати, якщо відчуваєте, що це потрібно
 
 */
 
-CREATE TABLE reustrants(
-    id SERIAL PRIMARY KEY
-)
-
-CREATE TABLE delivery_services(
-    id serial PRIMARY KEY
-)
-
-CREATE TABLE restaurants_to_deliveries(
-    restaurant_id int REFERENCES reustrants(id),
-    delivery_id int REFERENCES delivery_services(id),
-    PRIMARY KEY(restaurant_id, delivery_id)
-)
-
-INSERT INTO restaurants_to_deliveries VALUES
-(1, 1, 'pepperoni'),
-(1, 1, 'sea'),
-(1, 1, '4chease'),
-(1, 1, 'hawaii'),
-(1, 2, 'pepperoni'),
-(1, 2, 'sea'),
-(1, 2, 'chief'),
-(1, 3, 'pepperoni');
-
-
-CREATE TABLE pizzas(
-    name varchar(200) PRIMARY KEY
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    name varchar(300) CHECK (name != '') NOT NULL
 );
 
-CREATE TABLE pizzas_to_restaurants(
-    pizza_type varchar(300),
-    restaurant_id int REFERENCES reustrants(id),
-    PRIMARY KEY(pizza_type, restaurant_id) 
+CREATE TABLE manufacturers(
+    id serial PRIMARY KEY,
+    name varchar(400) CHECK (name != '') NOT NULL,
+    address text CHECK (address != '') NOT NULL,
+    tel_number varchar(20) CHECK (tel_number != '') NOT NULL
+);
+
+CREATE TABLE orders(
+    id serial PRIMARY KEY,
+    product_id int REFERENCES products(id),
+    contract_address text CHECK (contract_address != '') NOT NULL,
+    quantity_plan int NOT NULL,
+    product_price decimal(10, 2),
+    contract_number int NOT NULL,
+    contract_date date NOT NULL,
+    manufacturer_id int REFERENCES manufacturers(id)
+);
+
+CREATE TABLE shipments(
+    id serial PRIMARY KEY,
+    order_id int REFERENCES orders(id),
+    shipment_date timestamp NOT NULL
+);
+
+CREATE TABLE products_to_shipments(
+    product_id int REFERENCES products(id),
+    shipment_id int REFERENCES shipments(id),
+    product_quantity int NOT NULL
 );
