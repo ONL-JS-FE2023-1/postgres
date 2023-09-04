7ce2e94e0e0e29788402132625f71d5702cef63f
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-04 20:28:21 +0300

    Additional practice

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,88 +1,99 @@
--- Задача: дізнатись, чи є в таблиці users користувачі, у яких в полі gender - NULL
-
-SELECT * FROM users
-WHERE gender IS NULL;
-
-
--- Задача. Створити стовпець в таблиці orders: виконано / не виокнано замовлення
-ALTER TABLE orders
-ADD COLUMN status boolean;
-
-UPDATE orders
-SET status = true
-WHERE id % 2 = 0;
-
-
-UPDATE orders
-SET status = false
-WHERE id % 2 = 1;
-
-
-SELECT id, created_at, customer_id, status AS order_status
-FROM orders; ---- alias for attribute
-
-
-------------CASE
-
-
----1 syntax----
-/*
-
-CASE
-    WHEN condition1 = true THEN result1
-    WHEN condition2 = true THEN result2
-    .....
-    ELSE result3
-END
-
-*/
-
-/* ЗАДАЧА: Вивести всі замовлення, там де статус "true" - написати "виконано"
-   а якщо false - написати "нове" */
-
-SELECT id, created_at, customer_id, status, (
-    CASE
-        WHEN status = TRUE THEN 'виконано'
-        WHEN status = FALSE THEN 'нове замовлення'
-        ELSE 'інший статус'
-    END
-) AS order_status
-FROM orders
-ORDER BY id;
-
-
-------2 syntax------
-/*
-CASE condition
-    WHEN value1 THEN result1
-    WHEN value2 THEN result2
-    .........
-    ELSE default_result
-END
-
-*/
-
-/*
-ЗАДАЧА: Витягти місяць народження юзера і на його основі вивести, народився юзер восени
-, навесні, влітку чи взимку
-*/
+/* Вивести юзерів, в яких в полі "стать" буде українською написано "жінка"
+або "чоловік" або "інше значення" */
 
 
 SELECT *, (
-    CASE extract('month' from birthday)
-        WHEN 1 THEN 'winter'
-        WHEN 2 THEN 'winter'
-        WHEN 3 THEN 'spring'
-        WHEN 4 THEN 'spring'
-        WHEN 5 THEN 'spring'
-        WHEN 6 THEN 'summer'
-        WHEN 7 THEN 'summer'
-        WHEN 8 THEN 'summer'
-        WHEN 9 THEN 'fall'
-        WHEN 10 THEN 'fall'
-        WHEN 11 THEN 'fall'
-        WHEN 12 THEN 'winter'
-        ELSE 'unkown'
+    CASE gender
+        WHEN 'female' THEN 'жінка'
+        WHEN 'male' THEN 'чоловік'
+        ELSE 'інше значення'
+    END
+) AS "стать"
+FROM users;
+
+
+/*
+Вивести на основі кількості років користувача, що він повнолітній або неповнолітній
+*/
+
+SELECT *, (
+    CASE
+        WHEN ( extract(year from age(birthday)) ) < 18 THEN 'неповнолітній'
+        WHEN ( extract(year from age(birthday)) ) >= 18 THEN 'повнолітній'
+        ELSE 'невідомо'
     END
 )
 FROM users;
+
+
+/*
+
+
+Вивести всі телефони (з таблиці products),
+якщо ціна телефону > 6 тис - "флагман",
+якщо ціна від 2 до 6 - "середній клас",
+якщо ціна <2 тис - "бюджетний"
+
+*/
+
+SELECT *, (
+    CASE
+        WHEN price > 6000 THEN 'Флагман'
+        WHEN price BETWEEN 2000 AND 6000 THEN 'Середній клас'
+        WHEN price < 2000 THEN 'Бюджетний'
+        ELSE 'Невідомо'
+    END
+) AS "статус_товара"
+FROM products;
+
+
+/*
+
+Вивести користувачів з інформацією про їхні замовлення у вигляді:
+- якщо більше >=3 - "постійний клієнт"
+- якщо від 1 до 2 - "активний клієнт"
+- якщо 0 замовлень - "новий клієнт"
+*/
+
+SELECT u.id, u.email, (
+    CASE
+        WHEN count(o.id) >= 3 THEN 'Постійний клієнт'
+        WHEN count(o.id) BETWEEN 1 AND 2 THEN 'Активний клієнт'
+        WHEN count(o.id) = 0 THEN 'Новий клієнт'
+    END
+) FROM
+users AS u JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id, u.email;
+
+----------
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    gender,
+    is_subscribe,
+    birthday
+  )
+VALUES (
+    'Iron',
+    'Man',
+    'tonymail@gmail.com',
+    'male',
+    true,
+    '1975-08-09'
+);
+
+
+SELECT u.id, u.email, (
+    CASE
+        WHEN count(o.id) >= 3 THEN 'Постійний клієнт'
+        WHEN count(o.id) BETWEEN 1 AND 2 THEN 'Активний клієнт'
+        WHEN count(o.id) = 0 THEN 'Новий клієнт'
+    END
+) FROM
+users AS u LEFT JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id, u.email
+ORDER BY count(o.id);
