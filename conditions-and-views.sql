e68f1a2a169351fbd0ed087285f6907ef30f7f4d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-05 21:11:07 +0300

    Functions

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -3,60 +3,45 @@
 -- Завдання виконайте, створивши вьюху
 
 
-
-
-
----VIEWS
---Нам потрібні всі юзери. Нам потрібно знати,скільки кожен юзер зробив замовлень
+--FUNCTIONS
 
 /*
+Життєвий цикл функції:
+1. Ви описуєте функцію, створюєте її.
+2. Ви можете за необхідності виконати функцію - її код буде виконано.
+*/
 
-CREATE VIEW name AS (
-    query....
-)
+/* СИНТАКСИС:
+
+CREATE FUNCTION function_name (parametr1 data_type, parametr2 data_type)
+RETURNS return_data_type
+AS
+$$
+BEGIN
+    ---- ТІЛО ФУНКЦІЇ (операції та обчислення)
+    RETURN ...result....; 
+END;
+$$
+LANGUAGE plpgsql; -- Мова програмування функції (це приклад для PostgreSQL)
 
 */
 
-CREATE OR REPLACE VIEW users_with_orders_amount AS (
-    SELECT u.*, count(o.id) AS "orders_amount" FROM
-    users AS u LEFT JOIN orders AS o
-    ON u.id = o.customer_id
-    GROUP BY u.id, u.email
-    ORDER BY "orders_amount" ASC
-);
+-- Задача:створити функцію, яка приймає 2 числа і додає їх
 
-DROP VIEW users_with_orders_amount;
-
-SELECT * FROM users_with_orders_amount;
-
---Витягти всі email юзерів, які мають тільки 1 замовлення
-SELECT email, orders_amount FROM users_with_orders_amount
-WHERE orders_amount = 1;
+CREATE FUNCTION add_numbers(a integer, b integer)
+RETURNS integer
+AS
+$$
+BEGIN
+    RETURN a + b;
+END;
+$$
+LANGUAGE plpgsql;
 
 
---Створити представлення, яке зберігає замовлення з їхньою вартістю
+---- виклик функції:
+SELECT add_numbers(2, 3) AS "sum"; -- 5
+SELECT add_numbers('a', 25); -- error 
 
-CREATE VIEW orders_with_price AS (
-    SELECT o.id AS "order_id", o.customer_id AS "customer_id", sum(p.price * otp.quantity) AS "order_sum", status FROM
-    orders AS o INNER JOIN orders_to_products AS otp
-    ON o.id = otp.order_id
-    INNER JOIN products AS p
-    ON p.id = otp.product_id
-    GROUP BY o.id
-);
-
---На основі вьюхи orders_with_price
---Вивести юзерів з сумою коштів, які вони витратили у нашому магазині
-
-CREATE VIEW users_with_total_amounts AS (
-    SELECT u.*, sum(owp.order_sum) AS "total_amount" FROM
-    users AS u INNER JOIN orders_with_price AS owp
-    ON u.id = owp.customer_id
-    GROUP BY u.id
-);
-
-
---Зробити топ-10 користувачів, які залишили найбільшу суму грошей у нашому магазині
-SELECT * FROM users_with_total_amounts
-ORDER BY total_amount DESC
-LIMIT 10;
+---Видалення функцій
+DROP FUNCTION add_numbers(integer, integer);
