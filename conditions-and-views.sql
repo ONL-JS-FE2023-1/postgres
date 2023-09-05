7dc25170abad900faf77c24afca13804f245ede6
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-05 20:09:52 +0300

    Additional examples

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,5 +1,6 @@
 --- Знайти всі телефони, які купував юзер з id 23
 -- Не використовуючи JOINів, за допомогою виразів підзапитів
+-- Завдання виконайте, створивши вьюху
 
 
 
@@ -31,3 +32,31 @@ SELECT * FROM users_with_orders_amount;
 --Витягти всі email юзерів, які мають тільки 1 замовлення
 SELECT email, orders_amount FROM users_with_orders_amount
 WHERE orders_amount = 1;
+
+
+--Створити представлення, яке зберігає замовлення з їхньою вартістю
+
+CREATE VIEW orders_with_price AS (
+    SELECT o.id AS "order_id", o.customer_id AS "customer_id", sum(p.price * otp.quantity) AS "order_sum", status FROM
+    orders AS o INNER JOIN orders_to_products AS otp
+    ON o.id = otp.order_id
+    INNER JOIN products AS p
+    ON p.id = otp.product_id
+    GROUP BY o.id
+);
+
+--На основі вьюхи orders_with_price
+--Вивести юзерів з сумою коштів, які вони витратили у нашому магазині
+
+CREATE VIEW users_with_total_amounts AS (
+    SELECT u.*, sum(owp.order_sum) AS "total_amount" FROM
+    users AS u INNER JOIN orders_with_price AS owp
+    ON u.id = owp.customer_id
+    GROUP BY u.id
+);
+
+
+--Зробити топ-10 користувачів, які залишили найбільшу суму грошей у нашому магазині
+SELECT * FROM users_with_total_amounts
+ORDER BY total_amount DESC
+LIMIT 10;
