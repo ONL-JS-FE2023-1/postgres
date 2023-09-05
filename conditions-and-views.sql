2164d00dd95fda4f43be5b8260fa674de54045fa
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-05 19:21:49 +0300

    Views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,62 +1,33 @@
---Підзапити--
-
--- IN, NOT IN, SOME, ANY, EXISTS, ALL
-
---Задача: знайти всіх користувачів, які робили замовлення
--- Не використовувати JOIN, не використовувати будь-яких реляційних операцій
-
-SELECT * FROM users AS u
-WHERE u.id IN (SELECT customer_id FROM orders);
-
---Задача: знайти всіх користувачів, які не робили замовлень
-
-SELECT * FROM users AS u
-WHERE u.id NOT IN (SELECT customer_id FROM orders);
-
---Задача: знайти телефони, які ніхто не замовляв
-
-SELECT * FROM products AS p
-WHERE p.id NOT IN (SELECT product_id FROM orders_to_products);
+--- Знайти всі телефони, які купував юзер з id 23
+-- Не використовуючи JOINів, за допомогою виразів підзапитів
 
 
 
--------EXISTS
-
---Задача: дізнатись,чи є в мене в таблиці юзер з id 4000
-
-SELECT EXISTS 
-(SELECT * FROM users
-WHERE id = 4000);
 
 
----Задача: використовуючи EXISTS знайти всіх користувачів, які робили замовлення
+---VIEWS
+--Нам потрібні всі юзери. Нам потрібно знати,скільки кожен юзер зробив замовлень
 
-SELECT * FROM users AS u
-WHERE EXISTS (
-    SELECT * FROM orders AS o
-    WHERE u.id = o.customer_id
+/*
+
+CREATE VIEW name AS (
+    query....
+)
+
+*/
+
+CREATE OR REPLACE VIEW users_with_orders_amount AS (
+    SELECT u.*, count(o.id) AS "orders_amount" FROM
+    users AS u LEFT JOIN orders AS o
+    ON u.id = o.customer_id
+    GROUP BY u.id, u.email
+    ORDER BY "orders_amount" ASC
 );
 
----Задача: дізнатись, чи робив якийсь користувач замовлення. Відповідь подати у форматі true/false
-SELECT EXISTS 
-(SELECT * FROM orders
-WHERE customer_id = 994);
+DROP VIEW users_with_orders_amount;
 
+SELECT * FROM users_with_orders_amount;
 
---ANY/SOME
---SOME--ANY--IN <<---- СИНОНІМИ
-
---Задача: знайти всіх користувачів,які робили замовлення
-
-SELECT * FROM users AS u
-WHERE u.id = ANY (SELECT customer_id FROM orders);
-
-SELECT * FROM users AS u
-WHERE u.id = SOME (SELECT customer_id FROM orders);
-
-
----ALL
-
---Задача: Знайти всі телефони, які ніхто не купував
-SELECT * FROM products AS p
-WHERE p.id != ALL(SELECT product_id FROM orders_to_products);
+--Витягти всі email юзерів, які мають тільки 1 замовлення
+SELECT email, orders_amount FROM users_with_orders_amount
+WHERE orders_amount = 1;
