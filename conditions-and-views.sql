57e932b69ea207bedc0c3781698489a694794424
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-06 18:27:04 +0300

    Task resolve

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,47 +1,21 @@
---- Знайти всі телефони, які купував юзер з id 23
+--- Знайти всі телефони, які купував юзер з id 11
 -- Не використовуючи JOINів, за допомогою виразів підзапитів
--- Завдання виконайте, створивши вьюху
+
+-- варіант без JOIN'ів
+SELECT * FROM products AS p
+WHERE p.id = ANY (
+    SELECT product_id FROM orders_to_products AS otp 
+    WHERE otp.order_id = SOME (
+        SELECT id FROM orders AS o
+        WHERE customer_id = 11
+    )
+);
 
 
---FUNCTIONS
-
-/*
-Життєвий цикл функції:
-1. Ви описуєте функцію, створюєте її.
-2. Ви можете за необхідності виконати функцію - її код буде виконано.
-*/
-
-/* СИНТАКСИС:
-
-CREATE FUNCTION function_name (parametr1 data_type, parametr2 data_type)
-RETURNS return_data_type
-AS
-$$
-BEGIN
-    ---- ТІЛО ФУНКЦІЇ (операції та обчислення)
-    RETURN ...result....; 
-END;
-$$
-LANGUAGE plpgsql; -- Мова програмування функції (це приклад для PostgreSQL)
-
-*/
-
--- Задача:створити функцію, яка приймає 2 числа і додає їх
-
-CREATE FUNCTION add_numbers(a integer, b integer)
-RETURNS integer
-AS
-$$
-BEGIN
-    RETURN a + b;
-END;
-$$
-LANGUAGE plpgsql;
-
-
----- виклик функції:
-SELECT add_numbers(2, 3) AS "sum"; -- 5
-SELECT add_numbers('a', 25); -- error 
-
----Видалення функцій
-DROP FUNCTION add_numbers(integer, integer);
+--варіант з JOIN'ами
+SELECT * FROM products AS p
+INNER JOIN orders_to_products AS otp
+ON otp.product_id = p.id
+INNER JOIN orders AS o
+ON otp.order_id = o.id
+WHERE o.customer_id = 11;
