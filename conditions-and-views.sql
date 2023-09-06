6b8453ea14a9527f00f9046a29145efbe4dbb8a6
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-06 20:04:33 +0300

    Create schema

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,50 +1,7 @@
-/* ENUM */
+CREATE SCHEMA newtask;
 
----order status:
--- true - виконано
--- false - не виконано
+---Задача: створити таблицю users у схемі newtask
 
-SELECT * FROM orders
-WHERE status = true;
-
-
---- 'processing' --- 'prosesing' --- '...' --- '...'
-
----new
----processing
----shiped
----done
-
-
-CREATE TYPE order_status AS ENUM('new', 'processing', 'shiped', 'done');
-
----
-
-ALTER TABLE orders
-ALTER COLUMN status
-TYPE order_status
-USING(
-    CASE status
-        WHEN false THEN 'processing'
-        WHEN true THEN 'done'
-        ELSE 'new'
-    END
-)::order_status;
-
-
-----
-DROP VIEW orders_with_price, users_with_orders_amount, users_with_total_amounts;
-
-----
-
-INSERT INTO orders (customer_id, status)
-VALUES (
-    90,
-    'new'
-);
-
-
-------
-UPDATE orders
-SET status = 'shiped'
-WHERE id = 6;
+CREATE TABLE newtask.users(
+    --....
+)
